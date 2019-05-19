0cd1c27389dd827ee36e3288b46debab7da195e4
PeterYegorov <43631802+PeterYegorov@users.noreply.github.com>
2019-05-19 16:02:49 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -1,10 +1,4 @@
-#include <fstream>
-#include <iostream>
-#include <string>
 
-void print(const std::string& text, std::ofstream& out);
-void print(const std::string& text, std::ofstream& out);
-cat > include/print.hpp <<EOF
 #include <fstream>
 #include <iostream>
 #include <string>
